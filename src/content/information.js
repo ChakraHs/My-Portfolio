e874dc781736f7647d227e20566b7190dd196aec
Chakra Hs <lahoucine_chakra@um5.ac.ma>
2024-03-09 19:18:20 +0100

    change image

--- src/content/information.js
+++ src/content/information.js
@@ -1,10 +1,11 @@
+import imagePortfolio from'../assets/img/prtfolio_image.png';
 //personal information start
 
 const userData = {
   firstName: "<Chakra",
   lastName: "Hussein/>",
   title: "About Me",
-  img: "https://pics.craiyon.com/2023-10-30/66ff0c95358e458c859ecea5d3764b32.webp",
+  img: imagePortfolio,
   description:
     "Hey there! 👋 I'm Hussein, a passionate Software Engineering undergrad at Ensias School. 💻 By day, I'm a tech enthusiast exploring the wonders of coding, and by night, I transform into an aspiring full-stack developer. 🌌 My journey spans from crafting code to delving into the realms of IoT, cybersecurity, AI, and ML. When not in the digital world, you'll find me navigating the strategic landscape of League of Legends for some competitive fun. Let's connect and embark on an exciting adventure through the vast universe of technology! 🚀 ",
 };
